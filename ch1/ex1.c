52af6118b0c929b0bbf3b7bcd1a6f48b9ccf943c
daniel <adeyemodaniel10@gmail.com>
2017-06-01 21:42:10 +0100

    do first exercise

---
+++ ch1/ex1.c
@@ -0,0 +1,12 @@
+include "stdio.h"
+
+/* run the "hello, world" program on your system. Experiment
+	with leaving out parts of the program, to see what error messages you get*/
+
+int main()
+{
+	printf("hello, world\n");
+	// ("hello, world")
+	// printf("hello, world")
+	return 0;
+}
